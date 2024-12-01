cbf2724bb3b38495f2b1a6a3061260f5b0a0706b
lukepulverenti <3607463+LukePulverenti@users.noreply.github.com>
2024-11-30 20:31:56 -0500

    removed dead code

--- MediaBrowser.Plugins.Anime/Providers/AniDB/Metadata/AniDbPersonInfo.cs
+++ MediaBrowser.Plugins.Anime/Providers/AniDB/Metadata/AniDbPersonInfo.cs
@@ -8,14 +8,4 @@ namespace MediaBrowser.Plugins.Anime.Providers.AniDB.Metadata
         public string Id { get; set; }
         public string Image { get; set; }
     }
-
-    public class CastList
-    {
-        public List<AniDbPersonInfo> Cast { get; set; }
-
-        public CastList()
-        {
-            Cast = new List<AniDbPersonInfo>();
-        }
-    }
 }
